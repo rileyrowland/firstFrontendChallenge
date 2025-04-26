551f35a5ddc05a06dea434d59848ba2fdd052dd6
Riley Rowland <riley.scott.rowland@gmail.com>
2025-04-26 13:36:37 -0600

    updated header icon in dark mode

--- modebuttonmanager.js
+++ modebuttonmanager.js
@@ -1,5 +1,6 @@
 const themeToggleButton = document.getElementById("theme-toggle");
 const themeIcon = document.getElementById("theme-icon");
+const headerImage = document.querySelector("header img");
 
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
@@ -11,6 +12,11 @@ if (savedTheme) {
     themeIcon.setAttribute(
         "alt",
         savedTheme === "dark" ? "Sun Icon" : "Moon Icon"
+    );
+
+    headerImage.setAttribute(
+        "src",
+        savedTheme === "dark" ? "assets/images/logo-dark.svg" : "assets/images/logo.svg"
     );
 }
 
@@ -24,6 +30,11 @@ themeToggleButton.addEventListener("click", () => {
     themeIcon.setAttribute(
         "alt",
         isDarkMode ? "Sun Icon" : "Moon Icon"
+    );
+
+    headerImage.setAttribute(
+        "src",
+        isDarkMode ? "assets/images/logo-dark.svg" : "assets/images/logo.svg"
     );
 
     localStorage.setItem("theme", isDarkMode ? "dark" : "light");
